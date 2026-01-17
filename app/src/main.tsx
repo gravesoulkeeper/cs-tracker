23abdede69376297144d23b1eea884d0d402cd4d
Grave Keeper <gravesoulkeeper@gmail.com>
2026-01-17 23:08:34 +0530

    update main.tsx file

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,12 @@
 import { createRoot } from "react-dom/client";
 
-import App from "./App.tsx";
+import { Provider } from "react-redux";
+import store from "@store";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+);
