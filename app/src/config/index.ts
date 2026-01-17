111a986e69a484d94caa2fa81620c3829226f2c3
Grave Keeper <gravesoulkeeper@gmail.com>
2026-01-17 22:58:06 +0530

    feat(config): add initial config

---
+++ app/src/config/index.ts
@@ -0,0 +1,5 @@
+const SERVER_URL: string = import.meta.env.VITE_SERVER_URL;
+const SERVER_VERSION: string = import.meta.env.VITE_SERVER_VERSION;
+const RECAPTCHA_SITE_KEY: string = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
+
+export { SERVER_URL, SERVER_VERSION, RECAPTCHA_SITE_KEY };
