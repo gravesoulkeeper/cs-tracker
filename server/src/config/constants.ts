88dd6a067169d4a9b14780010fb79261f904decd
Grave Keeper <gravesoulkeeper@gmail.com>
2026-01-17 23:36:02 +0530

    feat(config): add initial config

--- server/src/config/constants.ts
+++ server/src/config/constants.ts
@@ -1,12 +1,28 @@
 import "dotenv/config";
 
-const NODE_ENV = process.env.NODE_ENV == "production";
+const NODE_ENV: string = process.env.NODE_ENV ?? "development";
 
-const MONGODB_URL: string = NODE_ENV
-	? (process.env.MONGODB_URL ?? "mongodb://localhost:27017/tracker")
-	: "mongodb://localhost:27017/tracker";
+const PORT: number = parseInt(process.env.PORT ?? "6600");
+const CORS_ORIGIN: string = process.env.CORS_ORIGIN ?? "http://localhost:5173/";
 
-const CORS_ORIGIN: string = NODE_ENV ? (process.env.CORS_ORIGIN ?? "localhost") : "*";
-const PORT: number = NODE_ENV ? parseInt(process.env.PORT ?? "6600") : 6600;
+const MONGODB_URL: string = process.env.MONGODB_URL ?? "mongodb://localhost:27017/tracker";
 
-export { MONGODB_URL, CORS_ORIGIN, PORT };
+const RECAPTCHA_SECRET_KEY: string = process.env.RECAPTCHA_SECRET_KEY ?? "";
+
+const JWT_SECRET_KEY: string = process.env.JWT_SECRET_KEY ?? "secret-key";
+const ACCESS_TOKEN_LIMIT: number = parseInt(process.env.ACCESS_TOKEN_LIMIT ?? "900000");
+const REFRESH_TOKEN_LIMIT: number = parseInt(process.env.REFRESH_TOKEN_LIMIT ?? "864000000");
+
+const COOKIE_DOMAIN: string = process.env.COOKIE_DOMAIN ?? "http://localhost:5173/";
+
+export {
+	NODE_ENV,
+	PORT,
+	CORS_ORIGIN,
+	MONGODB_URL,
+	RECAPTCHA_SECRET_KEY,
+	JWT_SECRET_KEY,
+	ACCESS_TOKEN_LIMIT,
+	REFRESH_TOKEN_LIMIT,
+	COOKIE_DOMAIN,
+};
