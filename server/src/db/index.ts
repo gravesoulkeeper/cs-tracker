f7dd56d28255632c1545d29c10d3b2247cf2115f
Grave Keeper <gravesoulkeeper@gmail.com>
2026-01-17 23:31:04 +0530

    refactor(db): minor updates and improve console logging

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 import type { Mongoose } from "mongoose";
 
-import { MONGODB_URL } from "../config/constants.ts";
+import { MONGODB_URL } from "@config";
 
 export async function connectMongoDB(): Promise<void> {
 	try {
 		const connectionInstance: Mongoose = await mongoose.connect(MONGODB_URL);
 		console.log("MongoDB connection establish :", connectionInstance.connection.host);
-	} catch (err) {
+	} catch (err: any) {
 		console.error("MongoDB connection failed!\n", err.message);
 		process.exit(1);
 	}
