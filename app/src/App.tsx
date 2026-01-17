d5fb359d984ef924ea4b9ff094c11bb40fab149c
Grave Keeper <gravesoulkeeper@gmail.com>
2026-01-17 23:06:19 +0530

    feat: add routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,36 @@
+import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { useEffect } from "react";
+
+import router from "@routes";
+import { initSite } from "@utils";
+import { profile } from "@api";
+import { useAppDispatch, useAppSelector } from "./store";
+
 const App = () => {
+	const { is_authenticated } = useAppSelector((state) => state.auth);
+	const dispatch = useAppDispatch();
+	useEffect(() => {
+		initSite();
+		dispatch(profile());
+	}, [is_authenticated]);
+
 	return (
-		<div className="bg-[#8A0303] text-center font-medium text-xl text-white">
-			gravesoulkeeper: need blood!!!
-		</div>
+		<>
+			<RouterProvider router={router} />
+			<ToastContainer
+				position="top-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={true}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+			/>
+		</>
 	);
 };
 
